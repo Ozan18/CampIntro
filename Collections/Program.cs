04e785aeab7513fbfe5d2f6fcaf892ac16216684
berhan cakmak <alicakmak6234@gmail.com>
2021-04-03 23:45:36 +0300

    Update 1.1

---
+++ Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Engin" ,"Murat" , "Kerem" , "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler[4] = "ilker";
+            //Console.WriteLine(isimler[4]);
+            //ctrl+k ctrl+c = toplu komut satırı
+            //ctrl+k ctrl+u = toplu komut satırını geri alma
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Ahmet");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+            
+
+
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Added !");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Updated !");
+
+        }
+        public int Topla(int sayi1 , int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1+sayi2);
+        }
+    }
+}
